b22743e348630b61d643dc6ba98d89ee4a1e3b21
sipoufo <sipoufoknj@gmail.com>
2023-11-28 14:56:14 +0100

    first commit

---
+++ src/routes/RouteManagement.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Routes } from "react-router-dom";
+
+const RouteManagement = () => {
+    return (
+        <Routes>
+            
+        </Routes>
+    )
+}
+
+export default RouteManagement;
